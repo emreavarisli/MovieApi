ec2e0eb011e6f66fff59e094acc052b6a400f603
Hasan Emre AVARİSLİ <emreavarisli@gmail.com>
2025-03-08 08:41:30 +0300

    swagger eklenmesi

--- Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/GetMovieByIdQueryHandler.cs
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/GetMovieByIdQueryHandler.cs
@@ -22,6 +22,7 @@ namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
             var value = await _context.Movies.FindAsync(query.MovieId);
             return new GetMovieByIdQueryResults
             {
+                MovieId = value.MovieId,
                 CoverImageUrl = value.CoverImageUrl,
                 CreatedYear = value.CreatedYear,
                 Description = value.Description,
--- Presentation/MovieApi.WebApi/Program.cs
+++ Presentation/MovieApi.WebApi/Program.cs
@@ -1,9 +1,12 @@
 using MovieApi.Application.Features.CQRSDesignPattern.Handlers.CategoryHandlers;
 using MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers;
+using MovieApi.Persistence.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<MovieContext>();
 
 builder.Services.AddScoped<GetCategoryQueryHandler>();
 builder.Services.AddScoped<GetCategoryByIdQueryHandler>();
@@ -18,15 +21,22 @@ builder.Services.AddScoped<RemoveMovieCommandHandler>();
 builder.Services.AddScoped<UpdateMovieCommandHandler>();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "My Api", Version = "v1" });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(x =>
+    {
+        x.SwaggerEndpoint("/swagger/v1/swagger.json", "My Api V1");
+    }); 
 }
 
 app.UseHttpsRedirection();
